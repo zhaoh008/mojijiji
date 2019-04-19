2efadc2e14834817ecb52c4eb2b9744c53745298
黄昌喜 <2489163099@qq.com>
2019-04-19 09:28:25 +0800

    zhaohang gogo

--- application/common/exception/Http.php
+++ application/common/exception/Http.php
@@ -1,7 +1,26 @@
 <?php
 
+namespace app\common\exception;
 
-class Http
+use Exception;
+use think\exception\Handle;
+
+/**
+ * Class Http
+ * @package app\common\exception
+ * 异常类自定义
+ */
+class Http extends Handle
 {
+
+    public function render(\Exception $e){
+        if(config('app_debug')){
+            //如果开启debug则正常报错
+            return parent::render($e);
+        }else{
+            //404页面  自行定义
+            header("Location:".url('index/index/index'));
+        }
+    }
 
 }

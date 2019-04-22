ff2d44460cfa5a435fd867458a359b8e749a4923
黄昌喜 <2489163099@qq.com>
2019-04-22 13:53:37 +0800

    zhaohang gogo

--- application/index/controller/Category.php
+++ application/index/controller/Category.php
@@ -10,7 +10,7 @@ class Category extends Controller
 {
         public function categoryList(){
             $category_list=model('Category')->getCategory();
-            var_dump($category_list);die();
+            echo "<pre>";print_r($category_list);echo "<pre>";;die();
             foreach ($category_list as $key=>$value){
 
             }

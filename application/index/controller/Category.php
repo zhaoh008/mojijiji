8738d6c4214bef19360fb079ec51a39f47758a68
黄昌喜 <2489163099@qq.com>
2019-04-22 13:45:36 +0800

    zhaohang gogo

--- application/index/controller/Category.php
+++ application/index/controller/Category.php
@@ -9,7 +9,11 @@ use think\Controller;
 class Category extends Controller
 {
         public function categoryList(){
-            $category=model('Category')->getCategory();
+            $category_list=model('Category')->getCategory();
+            print_r($category_list);die();
+            foreach ($category_list as $key=>$value){
+
+            }
             $this->fetch('',['category'=>$category]);
         }
 }

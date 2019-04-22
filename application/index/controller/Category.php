0383a761541e37d0e3ab4a621602a679934cc6c2
黄昌喜 <2489163099@qq.com>
2019-04-22 14:10:04 +0800

    zhaohang gogo

--- application/index/controller/Category.php
+++ application/index/controller/Category.php
@@ -6,15 +6,22 @@ namespace app\index\controller;
 
 use think\Controller;
 
+/*
+ * 分类控制器
+ */
 class Category extends Controller
 {
+        /*
+         * 分类列表获取渲染
+         */
         public function categoryList(){
             $category_list=model('Category')->getCategory();
 //            echo "<pre>";print_r($category_list);echo "<pre>";;die();
+            //计算每个分类下的文章数量
             foreach ($category_list as $key=>$value){
+                $article_num=model('Article')->categoryArticleNum($category_list[$key]['id']);
                 $category_list[$key]['article_num']=3;
             }
-            echo "<pre>";print_r($category_list);echo "<pre>";;die();
-            $this->fetch('',['category'=>$category]);
+            $this->fetch('',['category'=>$category_list]);
         }
 }

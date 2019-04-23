5a84b713d4ca567f2a5098c01f0e13ac3e245580
黄昌喜 <2489163099@qq.com>
2019-04-23 10:25:41 +0800

    zhaohang gogo

---
+++ application/api/controller/Json.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace app\api\controller;
+
+/*
+ * 返回JSON类，用于前端测试
+ */
+
+use think\Controller;
+
+class Json extends Controller
+{
+            /*
+             * 返回json
+             */
+        public function index() {
+            $i=888;
+            $row=array();
+         for ($j=0;$j<=$i;$j++){
+             $name='name'.$j;
+             $price=2^$j;
+             $row=([
+                 'name'=>$name,
+                 'price'=>$price
+             ]);
+         }
+        print_r($row);
+        }
+}

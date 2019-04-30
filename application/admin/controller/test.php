3d93031f305d59378282e46eb9ceca55cf19689e
黄昌喜 <2489163099@qq.com>
2019-04-30 15:27:46 +0800

    超酷后台模板玩耍

---
+++ application/admin/controller/test.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *test.php
+ *  
+ * current file name
+ * Administrator
+ *  
+ * current user system login name
+ * 2019-04-30
+ *  
+ * current system date
+ * 15:27
+ *  
+ * current system time
+ * 2019
+ *  
+ * current year
+ * 04
+ *  
+ * current month
+ * 四月
+ *  
+ * first 3 letters of the current month name. Example: Jan, Feb, etc.
+ * 四月
+ *  
+ * full name of the current month. Example: January, February, etc.
+ * 30
+ *  
+ * current day of the month
+ * 15
+ *  
+ * current hour
+ * 27
+ *  
+ * current minute
+ * 21
+ *  
+ * current second
+ * mojijiji
+ *  
+ * the name of the current project
+ * PhpStorm
+ *  
+ * current IDE name
+ * mojijiji
+ *  
+ * current project name
+ *
+ *
+ *
+ *
+ *
+ */
+
+
+namespace app\admin\controller;
+
+
+class test
+{
+
+}

5abca815aa8a851917afcd22a0c3099687d649aa
赵航 <zhaoh008@gmail.com>
2019-05-05 15:34:53 +0800

    超酷后台模板玩耍

--- application/config.php
+++ application/config.php
@@ -241,6 +241,6 @@ return [
     'view_replace_str'  =>  [
         '__PUBLIC__'=>'/static',
         '__ROOT__' => '/',
-        '__COOLADMIN__'=>'/static/program-admin'
+        '__COOLADMIN__'=>'/static/cooladmin'
     ],
 ];
---
+++ application/cooladmin/controller/Login.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * 文件名 Login.php
+ * 创建者 赵航
+ * 邮箱 zhaoh008@gmail.com
+ * 创建时间 2019-05-05 15:12
+ * 项目名称 mojijiji
+ */
+
+
+namespace app\cooladmin\controller;
+
+
+use think\Controller;
+
+class Login extends  Controller
+{
+    /*
+     * 登录功能
+     */
+    public function Login(){
+        return $this->fetch();
+    }
+}
